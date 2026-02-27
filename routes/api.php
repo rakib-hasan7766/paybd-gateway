5460809b0737669d12b9f13cd7a9c82d142c690a
rakib-hasan7766 <service.infobg@gmail.com>
2026-02-27 16:34:58 +0600

    Delete routes directory

--- routes/api.php
+++
@@ -1,5 +0,0 @@
-use App\Http\Controllers\Api\PaymentApiController;
-use Illuminate\Support\Facades\Route;
-
-Route::post('/v1/create-payment', [PaymentApiController::class, 'create']);
-Route::post('/v1/verify-payment', [PaymentApiController::class, 'verify']);
